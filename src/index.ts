21594ac470d8de097a5489d0b7b441f93dedff2f
xurde prog <rawatkapish969073@gmail.com>
2025-01-14 16:02:51 +0530

    Product Router setup

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import productsRoutes from './routes/products/index';
 
 const port = 3000;
 
@@ -7,7 +8,13 @@ const app = express();
 app.get('/',(req, res) =>{
     res.send ('Hello World123')
 
-})
+});
+
+
+
+app.use('/products', productsRoutes);
+
+
 app.listen(port,()=>{
     console.log(`Example app listening on port ${port}`)
 })
